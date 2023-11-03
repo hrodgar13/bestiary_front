f1db9c88c495089ccf38758be3e8aa31655df7b7
Illia Yermackov <ermackov2003@gmail.com>
2023-11-03 09:45:22 +0100

    get interface is ready

--- src/app/pages/bestiary/beast-page/beast-page.component.ts
+++ src/app/pages/bestiary/beast-page/beast-page.component.ts
@@ -3,6 +3,63 @@ import {DestroySubscription} from "../../../../shared/helpers/destroy-subscribti
 import {BestiaryService} from "../bestiary.service";
 import {takeUntil} from "rxjs";
 import {ActivatedRoute, Router} from "@angular/router";
+import {CreateTranslationAttribute} from "../../../../shared/interfaces/creature/create-attribute.interface";
+
+export interface Creature {
+  abilities: Action[]
+  actions: Action[]
+  alignment: OneAttribute
+  armorClass: number
+  armorTag: OneAttribute
+  bonusActions: Action
+  charisma: number
+  conditionsImmunities: Measure[]
+  construction: number
+  creatureName: CreateTranslationAttribute
+  dangerLevel: number
+  description: CreateTranslationAttribute
+  dexterity: number
+  experience: string
+  feelings: Measure[]
+  hits: number
+  hitsInDice: string
+  id: number
+  immunities: Measure[]
+  intelligence: number
+  isFinished: boolean
+  languages: Measure[]
+  legendaryActions: Action[]
+  masteryBonus: number
+  regions: Measure[]
+  resists: Measure[]
+  savingThrows: Measure[]
+  size: OneAttribute
+  skills: Measure[]
+  speeds: Measure[]
+  strength: number
+  type: OneAttribute
+  vulnerabilities: Measure[]
+  wisdom: number
+}
+
+export interface Action {
+  id: number
+  title: CreateTranslationAttribute
+  description: CreateTranslationAttribute
+}
+
+export interface Measure {
+  id: number,
+  amt: number | null
+  isMeasureEnable: boolean | null
+  attribute: OneAttribute
+}
+
+
+export interface OneAttribute {
+  attrName: CreateTranslationAttribute
+  id: number
+}
 
 @Component({
   selector: 'app-beast-page',
@@ -10,6 +67,8 @@ import {ActivatedRoute, Router} from "@angular/router";
   styleUrls: ['./beast-page.component.scss']
 })
 export class BeastPageComponent extends DestroySubscription implements OnInit{
+
+  creature!: Creature
 
   constructor(
     private readonly bestiaryService: BestiaryService,
@@ -27,7 +86,9 @@ export class BeastPageComponent extends DestroySubscription implements OnInit{
 
   private getCreature(id: number) {
     this.bestiaryService.getCreatureById(id).pipe(takeUntil(this.destroyStream$)).subscribe(data => {
-      console.log(data)
+      this.creature = data
+
+      console.log(this.creature)
     })
   }
 }
