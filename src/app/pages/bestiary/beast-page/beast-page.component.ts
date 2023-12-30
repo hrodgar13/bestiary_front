fe2877c265cf2cd77b387711ba0088a2ce6594f5
Illia Yermackov <ermackov2003@gmail.com>
2023-12-30 19:14:35 +0200

    beast-page

--- src/app/pages/bestiary/beast-page/beast-page.component.ts
+++ src/app/pages/bestiary/beast-page/beast-page.component.ts
@@ -3,40 +3,74 @@ import {DestroySubscription} from "../../../../shared/helpers/destroy-subscribti
 import {BestiaryService} from "../bestiary.service";
 import {takeUntil} from "rxjs";
 import {ActivatedRoute, Router} from "@angular/router";
+import {Creature} from "../../../../shared/interfaces/creature/get/creature";
+import {AttributeCode} from "../../../../shared/static/creature/attributes.code";
+import {MeasureCode} from "../../../../shared/static/creature/measure.code";
+import {Measure} from "../../../../shared/interfaces/creature/get/measure";
+import {ActionAbilities} from "../../../../shared/static/creature/action-abilities.code";
+import {ActionsAbilities} from "../../../../shared/interfaces/creature/get/actions-abilities";
+
 @Component({
   selector: 'app-beast-page',
   templateUrl: './beast-page.component.html',
   styleUrls: ['./beast-page.component.scss']
 })
-export class BeastPageComponent extends DestroySubscription implements OnInit{
+export class BeastPageComponent extends DestroySubscription implements OnInit {
+  AttributeCode = AttributeCode;
+  MeasureCode = MeasureCode;
 
-  // creature!: Creature
-  //
-  // constructor(
-  //   private readonly bestiaryService: BestiaryService,
-  //   private readonly attribute_code: ActivatedRoute
-  // ) {
-  //   super();
-  // }
-  ngOnInit(): void {
-    // this.attribute_code.params.pipe(takeUntil(this.destroyStream$)).subscribe(param => {
-    //   if(param['id']) {
-    //     this.getCreature(param['id'])
-    //   }
-    // })
+  creature!: Creature
+
+  constructor(
+    private readonly bestiaryService: BestiaryService,
+    private readonly attribute_code: ActivatedRoute
+  ) {
+    super();
   }
 
-  // private getCreature(id: number) {
-  //   this.bestiaryService.getCreatureById(id).pipe(takeUntil(this.destroyStream$)).subscribe(data => {
-  //     this.creature = data
+  ngOnInit(): void {
+    this.attribute_code.params.pipe(takeUntil(this.destroyStream$)).subscribe(param => {
+      if (param['id']) {
+        this.getCreature(param['id'])
+      }
+    })
+  }
+
+  private getCreature(id: number) {
+    this.bestiaryService.getCreatureById(id).pipe(takeUntil(this.destroyStream$)).subscribe(data => {
+      this.creature = data
+
+      console.log(this.creature)
+    })
+  }
+
   //
-  //     console.log(this.creature)
-  //   })
-  // }
-  //
-  // calculateModificator(income: number): string {
-  //   const modif = (income - 10) / 2
-  //
-  //   return modif < 0 ? `${modif}` : `+${modif}`
-  // }
+  calculateModificator(income: number | null): string {
+    if(income) {
+      const modif = Math.floor((income - 10) / 2)
+
+      return modif < 0 ? `${modif}` : `+${modif}`
+    }
+
+    return '0'
+  }
+
+
+  defineValueByAttribute(attr: AttributeCode) {
+    return this.creature.attributes.find(item => item.attr_cat === attr)?.name['en']
+  }
+
+  defineCreatureMeasures(msr: MeasureCode): Measure[] {
+    return this.creature.measures.filter(item => item.measure_cat === msr)
+  }
+
+  defineActionAbilities(actionAbility: ActionAbilities): ActionsAbilities[] {
+    return this.creature.action_abilities.filter(item => item.action_type === actionAbility)
+  }
+
+  protected readonly ActionAbilities = ActionAbilities;
+
+  validateEmptyName(speed: Measure) {
+    return speed.attribute ? speed.attribute.name['en'] : ''
+  }
 }
--- src/app/pages/bestiary/create-beast/create-beast.component.ts
+++ src/app/pages/bestiary/create-beast/create-beast.component.ts
@@ -19,45 +19,45 @@ import {MatDialog} from "@angular/material/dialog";
 import {ActionAbilities} from "../../../../shared/static/creature/action-abilities.code";
 
 @Component({
-    selector: 'app-create-beast',
-    templateUrl: './create-beast.component.html',
-    styleUrls: ['./create-beast.component.scss']
+  selector: 'app-create-beast',
+  templateUrl: './create-beast.component.html',
+  styleUrls: ['./create-beast.component.scss']
 })
 export class CreateBeastComponent extends DestroySubscription implements OnInit, OnDestroy {
-    interval = interval(5 * 60 * 1000)
+  interval = interval(5 * 60 * 1000)
 
-    AttributesCodes = AttributeCode
-    MeasuresCodes = MeasureCode
-    ActionCodes = ActionAbilities
+  AttributesCodes = AttributeCode
+  MeasuresCodes = MeasureCode
+  ActionCodes = ActionAbilities
 
-    creatureId: null | number = null
-    creaturePayload!: Creature
+  creatureId: null | number = null
+  creaturePayload!: Creature
 
-    measures: CreateMeasure[] = []
-    actionAbilities: CreateActionAbility[] = []
+  measures: CreateMeasure[] = []
+  actionAbilities: CreateActionAbility[] = []
 
-    creatureForm!: UntypedFormGroup;
+  creatureForm!: UntypedFormGroup;
 
-    constructor(
-        private readonly formBuilder: FormBuilder,
-        private readonly creatureService: CreatureService,
-        private readonly matSnack: MatSnackBar,
-        private route: ActivatedRoute,
-        private router: Router,
-        private dialog: MatDialog
-    ) {
-        super()
-    }
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly creatureService: CreatureService,
+    private readonly matSnack: MatSnackBar,
+    private route: ActivatedRoute,
+    private router: Router,
+    private dialog: MatDialog
+  ) {
+    super()
+  }
 
-    ngOnInit(): void {
-      this.detectCreatureToUpdateId()
-      this.route.params.pipe(takeUntil(this.destroyStream$)).subscribe(param => {
-        this.creatureId = param['id']
-        this.getCreatureToUpdate()
-      })
+  ngOnInit(): void {
+    this.detectCreatureToUpdateId()
+    this.route.params.pipe(takeUntil(this.destroyStream$)).subscribe(param => {
+      this.creatureId = param['id']
+      this.getCreatureToUpdate()
+    })
 
-      this.startAutoSave()
-    }
+    this.startAutoSave()
+  }
 
   override ngOnDestroy() {
     super.ngOnDestroy();
@@ -65,189 +65,192 @@ export class CreateBeastComponent extends DestroySubscription implements OnInit,
   }
 
   private getCreatureToUpdate() {
-        if (this.creatureId) {
-            this.creatureService.getCreatureById(this.creatureId).pipe(takeUntil(this.destroyStream$)).subscribe(data => {
-                this.creaturePayload = data
+    if (this.creatureId) {
+      this.creatureService.getCreatureById(this.creatureId).pipe(takeUntil(this.destroyStream$)).subscribe(data => {
+        this.creaturePayload = data
 
-                this.measures = this.convertMeasureInCreationType(data.measures)
-                this.actionAbilities = this.convertActionAbilitiesInCreationType(data.action_abilities)
+        this.measures = this.convertMeasureInCreationType(data.measures)
+        this.actionAbilities = this.convertActionAbilitiesInCreationType(data.action_abilities)
 
-              this.initForm()
-            })
-        } else {
-          this.initForm()
-        }
+        this.initForm()
+      })
+    } else {
+      this.initForm()
+    }
+  }
+
+  private initForm() {
+    this.creatureForm = this.formBuilder.group({
+      name_en: [this.creaturePayload?.name.en, []],
+      name_ua: [this.creaturePayload?.name.ua || null],
+      armor_class: [this.creaturePayload?.armor_class || null],
+      hits: [this.creaturePayload?.hits || null],
+      hits_in_dice: [this.creaturePayload?.hits_in_dice || null],
+      danger_lvl: [this.creaturePayload?.danger_lvl || null],
+      experience: [this.creaturePayload?.experience || null],
+      mastery_bonus: [this.creaturePayload?.mastery_bonus || null],
+      strength: [this.creaturePayload?.stat_block.strength || null],
+      dexterity: [this.creaturePayload?.stat_block.dexterity || null],
+      constitution: [this.creaturePayload?.stat_block.constitution || null],
+      intelligence: [this.creaturePayload?.stat_block.intelligence || null],
+      wisdom: [this.creaturePayload?.stat_block.wisdom || null],
+      charisma: [this.creaturePayload?.stat_block.charisma || null],
+      description_en: [this.creaturePayload?.description.en || null],
+      description_ua: [this.creaturePayload?.description.ua || null],
+      alignment: [this.defineSingleAttribute(this.creaturePayload, AttributeCode.alignment)],
+      size: [this.defineSingleAttribute(this.creaturePayload, AttributeCode.size)],
+      type: [this.defineSingleAttribute(this.creaturePayload, AttributeCode.type)],
+    })
+  }
+
+  sendForm(finishedStatus?: boolean) {
+    if (this.creatureForm.invalid) {
+      return
     }
 
-    private initForm() {
-        this.creatureForm = this.formBuilder.group({
-            name_en: [this.creaturePayload?.name.en, []],
-            name_ua: [this.creaturePayload?.name.ua || null],
-            armor_class: [this.creaturePayload?.armor_class || null],
-            hits: [this.creaturePayload?.hits || null],
-            hits_in_dice: [this.creaturePayload?.hits_in_dice || null],
-            danger_lvl: [this.creaturePayload?.danger_lvl || null],
-            experience: [this.creaturePayload?.experience || null],
-            mastery_bonus: [this.creaturePayload?.mastery_bonus || null],
-            strength: [this.creaturePayload?.stat_block.strength || null],
-            dexterity: [this.creaturePayload?.stat_block.strength || null],
-            constitution: [this.creaturePayload?.stat_block.strength || null],
-            intelligence: [this.creaturePayload?.stat_block.strength || null],
-            wisdom: [this.creaturePayload?.stat_block.strength || null],
-            charisma: [this.creaturePayload?.stat_block.strength || null],
-            description_en: [this.creaturePayload?.description.en || null],
-            description_ua: [this.creaturePayload?.description.ua || null],
-            alignment: [this.defineSingleAttribute(this.creaturePayload, AttributeCode.alignment)],
-            size: [this.defineSingleAttribute(this.creaturePayload, AttributeCode.size)],
-            type: [this.defineSingleAttribute(this.creaturePayload, AttributeCode.type)],
+
+    if (finishedStatus === undefined) {
+      let finished = false
+
+      const dialogRef = this.dialog.open(ConfirmDialogComponent, {data: {message: 'Do you finish The Beast?'}})
+
+      dialogRef.afterClosed().pipe(takeUntil(this.destroyStream$)).subscribe(data => {
+        finished = data
+
+        const creaturePayload: CreateCreature = this.prepareCreaturePayload(finished)
+        this.createUpdateCreature(creaturePayload)
+
+      })
+
+    } else {
+      const creaturePayload: CreateCreature = this.prepareCreaturePayload(finishedStatus)
+      this.createUpdateCreature(creaturePayload)
+    }
+  }
+
+  private prepareCreaturePayload(finished: boolean): CreateCreature {
+    return {
+      name: {
+        id: this.creaturePayload?.id || undefined,
+        en: this.creatureForm.get('name_en')?.value,
+        ua: this.creatureForm.get('name_ua')?.value,
+      },
+      isFinished: finished,
+      armor_class: this.creatureForm.get('armor_class')?.value,
+      hits: this.creatureForm.get('hits')?.value,
+      hits_in_dice: this.creatureForm.get('hits_in_dice')?.value,
+      danger_lvl: this.creatureForm.get('danger_lvl')?.value,
+      experience: this.creatureForm.get('experience')?.value,
+      mastery_bonus: this.creatureForm.get('mastery_bonus')?.value,
+      stat_block: {
+        id: this.creaturePayload?.stat_block?.id || undefined,
+        strength: this.creatureForm.get('strength')?.value,
+        dexterity: this.creatureForm.get('dexterity')?.value,
+        constitution: this.creatureForm.get('constitution')?.value,
+        intelligence: this.creatureForm.get('intelligence')?.value,
+        wisdom: this.creatureForm.get('wisdom')?.value,
+        charisma: this.creatureForm.get('charisma')?.value
+      },
+      measures: this.measures,
+      attributes: [this.creatureForm.get('alignment')?.value, this.creatureForm.get('size')?.value, this.creatureForm.get('type')?.value],
+      action_abilities: this.actionAbilities,
+      description: {
+        en: this.creatureForm.get('description_en')?.value,
+        ua: this.creatureForm.get('description_ua')?.value,
+      }
+    }
+  }
+
+  private createUpdateCreature(creaturePayload: CreateCreature) {
+    if (!this.creatureId) {
+      this.creatureService.createCreature(creaturePayload).pipe(takeUntil(this.destroyStream$)).subscribe(data => {
+        this.creatureId = data.id
+
+        localStorage.setItem('creatureId', data.id)
+
+        this.matSnack.open('saved', 'ok', {
+          duration: 3000,
+          verticalPosition: "top"
         })
+      })
+    } else {
+      this.creatureService.patchCreature(this.creatureId, creaturePayload).pipe(takeUntil(this.destroyStream$)).subscribe(data => {
+        this.matSnack.open('saved', 'ok', {
+          duration: 3000,
+          verticalPosition: "top"
+        })
+      })
     }
 
-    sendForm(finishedStatus?: boolean) {
-        if (this.creatureForm.invalid) {
-            return
-        }
+  }
 
+  private defineSingleAttribute(creaturePayload: Creature, attr_cat: string): number | null {
+    return this.creaturePayload?.attributes.find(item => item.attr_cat === attr_cat)?.id || null
+  }
 
-        if(finishedStatus === undefined) {
-          let finished = false
+  private convertAttributeInCreationType(attributes: Attribute[]): number[] {
+    return attributes.map(attr => attr.id);
+  }
 
-          const dialogRef = this.dialog.open(ConfirmDialogComponent, {data: {message: 'Do you finish The Beast?'}})
+  private convertMeasureInCreationType(measures: Measure[]): CreateMeasure[] {
+    return measures.map(measure => {
+      const attribute: Attribute[] = measure.attribute ? [measure.attribute] : []
 
-          dialogRef.afterClosed().pipe(takeUntil(this.destroyStream$)).subscribe(data => {
-            finished = data
-
-            const creaturePayload: CreateCreature = this.prepareCreaturePayload(finished)
-            this.createUpdateCreature(creaturePayload)
-
-          })
-
-        }
-
-        else {
-          const creaturePayload: CreateCreature = this.prepareCreaturePayload(finishedStatus)
-          this.createUpdateCreature(creaturePayload)
-        }
-    }
-
-    private prepareCreaturePayload(finished: boolean): CreateCreature {
       return {
-        name: {
-          id: this.creaturePayload?.id || undefined,
-          en: this.creatureForm.get('name_en')?.value,
-          ua: this.creatureForm.get('name_ua')?.value,
-        },
-        isFinished: finished,
-        armor_class: this.creatureForm.get('armor_class')?.value,
-        hits: this.creatureForm.get('hits')?.value,
-        hits_in_dice: this.creatureForm.get('hits_in_dice')?.value,
-        danger_lvl: this.creatureForm.get('danger_lvl')?.value,
-        experience: this.creatureForm.get('experience')?.value,
-        mastery_bonus: this.creatureForm.get('mastery_bonus')?.value,
-        stat_block: {
-          id: this.creaturePayload?.stat_block?.id || undefined,
-          strength: this.creatureForm.get('strength')?.value,
-          dexterity: this.creatureForm.get('dexterity')?.value,
-          constitution: this.creatureForm.get('constitution')?.value,
-          intelligence: this.creatureForm.get('intelligence')?.value,
-          wisdom: this.creatureForm.get('wisdom')?.value,
-          charisma: this.creatureForm.get('charisma')?.value
-        },
-        measures: this.measures,
-        attributes: [this.creatureForm.get('alignment')?.value, this.creatureForm.get('size')?.value, this.creatureForm.get('type')?.value],
-        action_abilities: this.actionAbilities,
-        description: {
-          en: this.creatureForm.get('description_en')?.value,
-          ua: this.creatureForm.get('description_ua')?.value,
-        }
+        ...measure,
+        attribute: this.convertAttributeInCreationType(attribute)[0]
       }
+    })
+  }
+
+  private convertActionAbilitiesInCreationType(action_abilities: ActionsAbilities[]): CreateActionAbility[] {
+    const create_action_abilities: CreateActionAbility[] = []
+
+    action_abilities.forEach(item => {
+      create_action_abilities.push({
+        id: item.id,
+        action_type: item.action_type,
+        description: item.description,
+        title: item.title
+      })
+    });
+
+    return create_action_abilities
+  }
+
+  addMeasure($event: CreateMeasure) {
+    this.measures.push($event)
+  }
+
+  removeMeasure($event: CreateMeasure) {
+
+
+    const idx = this.measures.findIndex(item => item.attribute === $event.attribute && item.measure_cat === item.measure_cat)
+
+    console.log($event, this.measures)
+
+    if (idx !== -1) {
+      this.measures.splice(idx, 1)
+      console.log(this.measures)
     }
+  }
 
-    private createUpdateCreature(creaturePayload: CreateCreature) {
-      if(!this.creatureId) {
-        this.creatureService.createCreature(creaturePayload).pipe(takeUntil(this.destroyStream$)).subscribe(data => {
-          this.creatureId = data.id
+  addActionAbility($event: CreateActionAbility) {
+    this.actionAbilities.push($event)
+  }
 
-          localStorage.setItem('creatureId', data.id)
+  removeActionAbility($event: CreateActionAbility) {
+    const idx = this.actionAbilities.findIndex(item => item === $event)
 
-          this.matSnack.open('saved', 'ok', {
-            duration: 3000,
-            verticalPosition: "top"
-          })
-        })
-      } else {
-        this.creatureService.patchCreature(this.creatureId, creaturePayload).pipe(takeUntil(this.destroyStream$)).subscribe(data => {
-          this.matSnack.open('saved', 'ok', {
-            duration: 3000,
-            verticalPosition: "top"
-          })
-        })
-      }
-
+    if (idx !== -1) {
+      this.actionAbilities.splice(idx, 1)
     }
-
-    private defineSingleAttribute(creaturePayload: Creature, attr_cat: string): number | null {
-        return this.creaturePayload?.attributes.find(item => item.attr_cat === attr_cat)?.id || null
-    }
-
-    private convertAttributeInCreationType(attributes: Attribute[]): number[] {
-        return attributes.map(attr => attr.id);
-    }
-
-    private convertMeasureInCreationType(measures: Measure[]): CreateMeasure[] {
-        return measures.map(measure => {
-            const attribute: Attribute[] = measure.attribute ? [measure.attribute] : []
-
-            return {
-                ...measure,
-                attribute: this.convertAttributeInCreationType(attribute)[0]
-            }
-        })
-    }
-
-    private convertActionAbilitiesInCreationType(action_abilities: ActionsAbilities[]): CreateActionAbility[] {
-        const create_action_abilities: CreateActionAbility[] = []
-
-        action_abilities.forEach(item => {
-            create_action_abilities.push({
-                id: item.id,
-                action_type: item.action_type,
-                description: item.description,
-                title: item.title
-            })
-        });
-
-        return create_action_abilities
-    }
-
-    addMeasure($event: CreateMeasure) {
-        this.measures.push($event)
-    }
-
-    removeMeasure($event: CreateMeasure) {
-        const idx = this.measures.findIndex(item => item === $event)
-
-        if (idx !== -1) {
-            this.measures.splice(idx, 1)
-        }
-    }
-
-    addActionAbility($event: CreateActionAbility) {
-        this.actionAbilities.push($event)
-    }
-
-    removeActionAbility($event: CreateActionAbility) {
-        const idx = this.actionAbilities.findIndex(item => item === $event)
-
-        if (idx !== -1) {
-            this.actionAbilities.splice(idx, 1)
-        }
-    }
+  }
 
   private detectCreatureToUpdateId() {
     const creatureId = localStorage.getItem('creatureId')
 
-    if(creatureId) {
+    if (creatureId) {
       localStorage.removeItem('creatureId')
       this.router.navigate([`../bestiary/edit/${creatureId}`])
     }
