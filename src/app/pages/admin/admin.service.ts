8df062ba4b387aa9c244af50487ee13b5f386fcd
Illia Yermackov <ermackov2003@gmail.com>
2024-02-28 20:03:29 +0200

    filters edit

--- src/app/pages/admin/admin.service.ts
+++ src/app/pages/admin/admin.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import {AuthService} from "../../../shared/services/auth.service";
+import {ApiService} from "../../../shared/services/api.service";
 
 @Injectable()
 export class AdminService {
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private apiService: ApiService
   ) { }
 
   isAdmin() {
     return this.authService.isAdminAuthenticated()
+  }
+
+  getFilters() {
+    return this.apiService.getFilters()
   }
 }
--- src/app/pages/admin/components/attributes-list/attributes-list.component.ts
+++ src/app/pages/admin/components/attributes-list/attributes-list.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CreatureListFilter} from "../../../../../shared/interfaces/filters/creature-list-filter";
+import {DestroySubscription} from "../../../../../shared/helpers/destroy-subscribtion";
+import {AdminService} from "../../admin.service";
+import {takeUntil} from "rxjs";
+import {TranslocoService} from "@ngneat/transloco";
 
 @Component({
   selector: 'app-attributes-list',
   templateUrl: './attributes-list.component.html',
   styleUrls: ['./attributes-list.component.scss']
 })
-export class AttributesListComponent {
+export class AttributesListComponent extends DestroySubscription implements OnInit{
+  filters: CreatureListFilter[] = [];
+  currentLang: 'en' | 'ua' = 'en'
+
+
+  constructor(
+    private adminService: AdminService,
+    private localeService: TranslocoService
+  ) {
+    super();
+  }
+
+  ngOnInit() {
+    this.getFilters()
+    this.detectLanguageChange()
+  }
+
+  getFilters() {
+    this.adminService.getFilters().pipe(takeUntil(this.destroyStream$)).subscribe(data => {
+      this.filters = data
+    })
+  }
+
+  private detectLanguageChange() {
+    this.localeService.langChanges$.pipe(takeUntil(this.destroyStream$)).subscribe(data => {
+      const activeLang: 'en' | 'ua' | string = data
+
+
+      if(activeLang === 'en' ||activeLang === 'ua') {
+        this.currentLang = activeLang
+      }
+    })
+  }
 
 }
