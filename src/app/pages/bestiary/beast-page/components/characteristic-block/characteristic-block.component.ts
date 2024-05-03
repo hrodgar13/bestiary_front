6fcb43c4fddd7994fcf39678da1d3678f7a37259
Illia Yermackov <ermackov2003@gmail.com>
2024-05-03 13:09:28 +0300

    provided dice roll to others rolling places

--- src/app/pages/bestiary/beast-page/components/characteristic-block/characteristic-block.component.ts
+++ src/app/pages/bestiary/beast-page/components/characteristic-block/characteristic-block.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {DiceRollerService} from "../../../../../../shared/services/dice-roller.service";
 
 @Component({
   selector: 'app-characteristic-block',
@@ -9,10 +10,21 @@ export class CharacteristicBlockComponent {
   @Input() characteristicValue: number | null = 0;
   @Input() title: string = '';
 
+  modificator: number = 0
+
+  constructor(
+    private diceRoller: DiceRollerService
+  ) {
+  }
+
+  rollDice() {
+    this.diceRoller.rollDice(1, 20, this.modificator)
+  }
+
   calculateModificator(income: number | null): string {
     if(income) {
       const modif = Math.floor((income - 10) / 2)
-
+      this.modificator = modif
       return modif < 0 ? `${modif}` : `+${modif}`
     }
 
