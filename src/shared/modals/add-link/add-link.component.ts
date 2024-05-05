0f7961a52bcb2e462fc4654425fbeec8682b7a11
Illia Yermackov <ermackov2003@gmail.com>
2024-05-05 19:05:07 +0300

    uncomment

--- src/shared/modals/add-link/add-link.component.ts
+++ src/shared/modals/add-link/add-link.component.ts
@@ -32,19 +32,19 @@ export class AddLinkComponent  implements OnInit, OnDestroy {
   }
 
   updateHTML(): void {
-    // const regex = /{%\s*custom_font_style="([^"]*)"\s*custom_font_color="([^"]*)"\s*value="([^"]*)"\s*%}/;
-    // const match = this.editableText.match(regex);
-    // if (match) {
-    //   const styles = match[1].split(' ').reduce((styleString, style) => {
-    //     if (style === 'bold') styleString += 'font-weight: bold; ';
-    //     if (style === 'italic') styleString += 'font-style: italic; ';
-    //     if (style === 'underline') styleString += 'text-decoration: underline; ';
-    //     return styleString;
-    //   }, '');
-    //   const htmlString = `<span style="${styles} color: ${match[2]};">${match[3]}</span>`;
-    //   this.editableHTML = this.sanitizer.bypassSecurityTrustHtml(htmlString);
-    // } else {
-    //   this.editableHTML = '';
-    // }
+    const regex = /{%\s*custom_font_style="([^"]*)"\s*custom_font_color="([^"]*)"\s*value="([^"]*)"\s*%}/;
+    const match = this.editableText.match(regex);
+    if (match) {
+      const styles = match[1].split(' ').reduce((styleString, style) => {
+        if (style === 'bold') styleString += 'font-weight: bold; ';
+        if (style === 'italic') styleString += 'font-style: italic; ';
+        if (style === 'underline') styleString += 'text-decoration: underline; ';
+        return styleString;
+      }, '');
+      const htmlString = `<span style="${styles} color: ${match[2]};">${match[3]}</span>`;
+      this.editableHTML = this.sanitizer.bypassSecurityTrustHtml(htmlString);
+    } else {
+      this.editableHTML = '';
+    }
   }
 }
