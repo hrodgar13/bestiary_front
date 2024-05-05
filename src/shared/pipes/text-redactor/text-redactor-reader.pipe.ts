70869c8e3fbbe363a4f0b13eee8420ff21164fe6
Illia Yermackov <ermackov2003@gmail.com>
2024-05-05 21:45:42 +0300

    work finished

--- src/shared/pipes/text-redactor/text-redactor-reader.pipe.ts
+++ src/shared/pipes/text-redactor/text-redactor-reader.pipe.ts
@@ -13,7 +13,9 @@ export class TextRedactorReaderPipe implements PipeTransform {
       return '';
     }
 
-    const templateRegex = /{%\s*(roll_dice_amt="(\d+)"\s*roll_dice_side="(\d+)"\s*roll_bonus="(\d*)"|custom_font_style="([^"]*)"\s*custom_font_color="([^"]*)"\s*value="([^"]*)")\s*%}/g;
+    const templateRegex = /{%\s*(roll_dice_amt="(\d+)"\s*roll_dice_side="(\d+)"\s*roll_bonus="(\d*)"|custom_font_style="([^"]*)"\s*custom_font_color="([^"]*)"\s*value="([^"]*)"\s*external_link="([^"]*)")\s*%}/g;
+
+    console.log(value)
 
     return this.sanitizer.bypassSecurityTrustHtml(value.replace(templateRegex, (match, p1, amt, side, bonus, fontStyle, fontColor, textValue) => {
       if (amt) { // Dice roll formatting
@@ -22,7 +24,12 @@ export class TextRedactorReaderPipe implements PipeTransform {
         return `<span style="${diceStyle}" onclick="window.rollDiceGlobal(${amt}, ${side}, ${bonus})">${diceText}</span>`;
       } else { // Style formatting
         let styleString = this.buildStyleString(fontStyle, fontColor);
-        return `<span style="${styleString}">${textValue}</span>`;
+        if(match[4]) {
+          return `<a target="_blank" href="${match[4]}" style="${styleString}">${textValue}</a>`;
+        } else {
+          return `<span style="${styleString}">${textValue}</span>`;
+        }
+
       }
     }));
   }
