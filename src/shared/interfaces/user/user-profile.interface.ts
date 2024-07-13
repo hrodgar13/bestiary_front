305db42371994ac8f7a30f2ed40f04ee2cafa0c8
Illia Yermackov <ermackov2003@gmail.com>
2024-07-13 18:19:42 +0300

    profile with mock - ready

--- src/shared/interfaces/user/user-profile.interface.ts
+++ src/shared/interfaces/user/user-profile.interface.ts
@@ -10,7 +10,7 @@ export interface UserProfile {
 export const MOCK_USER_PROFILE: UserProfile = {
   id: 0,
   dateOfCreation: new Date(),
-  avatarUrl: 'https://i.pinimg.com/564x/07/cd/8e/07cd8e3e3b085f93557fcdf499974866.jpg',
+  avatarUrl: 'fbf548f3-63d9-47f9-92a2-ab5db50f9272-eafca6af9e30b2998e41301a00e9abdd.jpg',
   email: 'ermackov2003@gmail.com',
   subscription: {
     id: 0,
