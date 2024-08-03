1646d678433200687fff7e4a2f659ccc57311aee
Illia Yermackov <ermackov2003@gmail.com>
2024-08-03 21:13:30 +0300

    create list

--- src/app/pages/user/universe-settings/universe-constructor/paragraph-constructor/create-paragraph/create-list/create-list.component.ts
+++ src/app/pages/user/universe-settings/universe-constructor/paragraph-constructor/create-paragraph/create-list/create-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {ListMetadataParagraphInterface} from "../../../../../../../../shared/interfaces/universes/universe.interface";
 
 @Component({
   selector: 'app-create-list',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-list.component.scss']
 })
 export class CreateListComponent {
+  protected readonly String = String;
 
+  @Output() sendData = new EventEmitter<string>
+
+  listObject: ListMetadataParagraphInterface = {
+    listItems: [],
+    type: 'letter'
+  }
+
+  currentInput: string = '';
+
+  addElementToList() {
+    const text = this.currentInput
+
+    this.listObject.listItems.push(text)
+    this.sendData.emit(JSON.stringify(this.listObject))
+  }
+
+  setListStyle(type: 'letter' | 'number' | 'symbol') {
+    this.listObject.type = type
+    this.sendData.emit(JSON.stringify(this.listObject))
+  }
 }
